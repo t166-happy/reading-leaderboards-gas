42285466b6165eb9d5b2208ae755617f54daa8ad
james su <t166@mail.happy.tyc.edu.tw>
2025-09-24 15:26:55 +0800

    ci: deploy to GitHub Pages via Actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/reading-leaderboards-gas/',  // ⚠️ repo 名稱
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
+  base: '/reading-leaderboards-gas/',   // 一定要跟 repo 名稱相同
+  build: { outDir: 'dist', emptyOutDir: true }
 })
